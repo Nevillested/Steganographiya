6bc0195c9e029f068cfe009f76c8a2fce9739f23
Nevillested <olegsidnev@gmail.com>
2022-05-14 00:09:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Steganographiya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image image = Image.FromFile(@"путь");
+            System.IO.MemoryStream memoryStream = new System.IO.MemoryStream();
+            image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] b = memoryStream.ToArray();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
